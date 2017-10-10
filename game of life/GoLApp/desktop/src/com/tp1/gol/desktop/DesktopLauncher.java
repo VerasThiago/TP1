47e5279a38b755903bd9c9411008ffb3c84da02b
vitordullens <vitordullens2.0@gmail.com>
2017-10-10 02:16:00 +0000

    libGDX repo

---
+++ game of life/GoLApp/desktop/src/com/tp1/gol/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tp1.gol.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tp1.gol.GoL;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GoL(), config);
+	}
+}
