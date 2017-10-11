22fbfcceeb050cc115941ae8cf54cf0c2cf60de4
Giovanni M Guidini <giovanni.guidini@gmail.com>
2017-10-10 23:52:10 -0300

    Primeiro .jar

--- game of life/GoL_TemplateMethod/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "GoL_TemplateMethod"
-
-version := "0.1"
-
-scalaVersion := "2.12.3"
-        
--- game of life/GoL_TemplateMethod/src/main/scala/Board.scala
+++
@@ -1,5 +0,0 @@
-class Board(val width : Int = 10, val height : Int = 10) {
-
-  var universe = Array.ofDim[Cells](width, height)
-
-}
