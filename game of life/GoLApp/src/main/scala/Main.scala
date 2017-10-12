3441f2d3f13a91ffa1865d6821b698bff6e9e75e
vitordullens <vitordullens2.0@gmail.com>
2017-10-12 18:30:16 +0000

    jeito burro 100%

--- game of life/GoLApp/src/main/scala/Main.scala
+++ game of life/GoLApp/src/main/scala/Main.scala
@@ -1,9 +1,12 @@
 
-import scalafx.application.JFXApp
+import jdk.internal.org.objectweb.asm.commons.RemappingAnnotationAdapter
 
+import scala.util.Random
+import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
-
+import scalafx.scene.layout.GridPane
+import scalafx.scene.paint.Color
 import scalafx.scene.shape.{Circle, Line, Rectangle}
 
 
@@ -11,6 +14,7 @@ import scalafx.scene.shape.{Circle, Line, Rectangle}
 object Main extends JFXApp {
   stage = new JFXApp.PrimaryStage {
     title = "Game of Life"
+
     scene = new Scene (250, 280) {
       //linhas
       val linha0 = Line(0, 0, 250, 0)
@@ -25,15 +29,15 @@ object Main extends JFXApp {
       val linha9 = Line(0, 225, 250, 225)
       val linha10 = Line(0, 250, 250, 250)
       //colunas
-      val collum1 = Line(25, 0, 25, 250)
-      val collum2 = Line(50, 0, 50, 250)
-      val collum3 = Line(75, 0, 75, 250)
-      val collum4 = Line(100, 0, 100, 250)
-      val collum5 = Line(125, 0, 125, 250)
-      val collum6 = Line(150, 0, 150, 250)
-      val collum7 = Line(175, 0, 175, 250)
-      val collum8 = Line(200, 0, 200, 250)
-      val collum9 = Line(225, 0, 225, 250)
+      val column1 = Line(25, 0, 25, 250)
+      val column2 = Line(50, 0, 50, 250)
+      val column3 = Line(75, 0, 75, 250)
+      val column4 = Line(100, 0, 100, 250)
+      val column5 = Line(125, 0, 125, 250)
+      val column6 = Line(150, 0, 150, 250)
+      val column7 = Line(175, 0, 175, 250)
+      val column8 = Line(200, 0, 200, 250)
+      val column9 = Line(225, 0, 225, 250)
       //botoes
       val start = new Button("Start")
       start.layoutX = 0
@@ -44,17 +48,18 @@ object Main extends JFXApp {
       val next = new Button("Next")
       next.layoutX = 210
       next.layoutY = 255
+      GridPane
 
       content = List(
         //linhas
         linha0, linha1,linha2,linha3,linha4,linha5,linha6,linha7,linha8,linha9,linha10,
         //colunas
-        collum1, collum2, collum3,collum4,collum5,collum6,collum7,collum8, collum9,
+        column1, column2, column3,column4,column5,column6,column7,column8, column9,
         //botoes
         start, stop, next
-
       )
 
     }
+
   }
 }
