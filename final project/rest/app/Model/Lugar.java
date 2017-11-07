abe856b1dce463548c3e159cf842865b4e6c130e
VerasUnB <thiago.wortnnes@gmail.com>
2017-11-07 22:15:05 +0000

    Adding search page with some find tool

---
+++ final project/rest/app/Model/Lugar.java
@@ -0,0 +1,9 @@
+package Model;
+
+import play.mvc.Controller;
+
+public class Lugar extends Controller {
+    public static String Name;
+    public static int Rank;
+    public static String Andress;
+}
