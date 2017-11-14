76a6d5ba50d6efb79ca7b0eaf9adaae40be96cf9
VerasUnB <thiago.wortnnes@gmail.com>
2017-11-14 18:56:18 +0000

    Almost done scroll list & new var of infowindow on data.js

--- final project/rest/public/data.js
+++ final project/rest/public/data.js
@@ -1,9 +1,13 @@
-console.log('prindando do pqp');
-
-let sss = 'sssssss';
-
 
 let locations = [
+    ['Universidade de Brasília', -15.763189,-47.870754, 5],
+    ['Saude.com',-15.661522,-47.838764, 4],
+    ['Ondas Burger',-15.661427,-47.8371979, 3],
+    ['Açaí Casca',-15.756465,-47.891857, 2],
+    ['Essence Lounge Bar',-15.814684,-47.889614, 1]
+];
+
+let informations = [
     ['<div style="color: black;">'+
     '<div id="iw-container">' +
     '<div class="iw-title">Universidade de Brasília</div>' +
@@ -17,10 +21,21 @@ let locations = [
     '</div>' +
     '<div class="iw-bottom-gradient"></div>' +
     '</div>'+
-    '</div>',
-        -15.763189,-47.870754, 5],
-    ['Saude.com',-15.661522,-47.838764, 4],
-    ['Ondas Burger',-15.661427,-47.8371979, 3],
-    ['Açaí Casca',-15.756465,-47.891857, 2],
-    ['Essence Lounge Bar',-15.814684,-47.889614, 1]
+    '</div>',5],
+    ['<div style="color: black;">'+'<p>Essa é a Saude.com</p>'+'</div>',4],
+    ['<div style="color: black;">'+'<p>Esse é o Ondas Burger</p>'+'</div>',3],
+    ['<div style="color: black;">'+'<p>Essa é o Açaí Casca</p>'+'</div>',2],
+    ['<div style="color: black;">'+'<p>Essa é o Essence Lounge Bar</p>'+'</div>',1]
 ];
+
+
+
+
+
+
+
+
+
+
+
+
