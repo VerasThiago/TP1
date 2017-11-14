949ac6d3ae0e27415094c50c3de780716eddeeaf
VerasUnB <thiago.wortnnes@gmail.com>
2017-11-14 17:51:24 +0000

    Receiving markers data from pqp.js file

---
+++ final project/rest/public/js/miojo.js
@@ -0,0 +1,22 @@
+let xxx = [
+                                        ['<div style="color: black;">'+
+                                            '<div id="iw-container">' +
+                                            '<div class="iw-title">Universidade de Brasília</div>' +
+                                            '<div class="iw-content">' +
+                                            '<div class="iw-subTitle">Universidade</div>' +
+                                            '<img src="https://goo.gl/e25sJG" alt="Porcelain Factory of Vista Alegre" height="150" width="150">' +
+                                            '<p>A Universidade de Brasília é uma universidade pública federal brasileira, com sede em Brasília, no Distrito Federal. A instituição possui 4 campi, sendo estes em Brasília, Planaltina, Gama, Ceilândia e Paranoá.</p>' +
+                                            '<div class="iw-subTitle">Contacts</div>' +
+                                            '<p>UnB - Brasília, DF, 70910-900'+
+                                            '<br>Phone.  (61) 3107-3300</p>'+
+                                            '</div>' +
+                                            '<div class="iw-bottom-gradient"></div>' +
+                                            '</div>'+
+                                            '</div>',
+                                                    -15.763189,-47.870754, 5],
+                                        ['Saude.com',-15.661522,-47.838764, 4],
+                                        ['Ondas Burger',-15.661427,-47.8371979, 3],
+                                        ['Açaí Casca',-15.756465,-47.891857, 2],
+                                        ['Essence Lounge Bar',-15.814684,-47.889614, 1]
+                                    ];
+let bit = 'deu MUITO errado';
