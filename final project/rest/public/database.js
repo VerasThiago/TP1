4b0cc144264790988255896dad3453f2e3c3f0c9
VerasUnB <thiago.wortnnes@gmail.com>
2017-11-15 20:54:38 +0000

    Starting map with user location & Custom Icons

---
+++ final project/rest/public/database.js
@@ -0,0 +1,49 @@
+        
+    let icons = [ // 0 bar, 1 Restaurant, 2 University, 3 User
+        ['http://maps.google.com/mapfiles/kml/pal2/icon19.png',0],
+        ['http://maps.google.com/mapfiles/kml/pal2/icon55.png',1],
+        ['http://icons.iconarchive.com/icons/graphicloads/100-flat/32/student-icon.png',2],
+        ['http://maps.google.com/mapfiles/kml/pal3/icon32.png',3]
+    ];
+    
+    let locations = [
+        ['Universidade de Brasília', -15.763189,-47.870754, 2],
+        ['Saude.com',-15.661522,-47.838764, 1],
+        ['Ondas Burger',-15.661427,-47.8371979, 1],
+        ['Açaí Casca',-15.756465,-47.891857, 1],
+        ['Essence Lounge Bar',-15.814684,-47.889614, 0],
+    ];
+
+    let informations = [
+        ['<div style="color: black;">'+
+        '<div id="iw-container">' +
+        '<div class="iw-title">Universidade de Brasília</div>' +
+        '<div class="iw-content">' +
+        '<div class="iw-subTitle">Universidade</div>' +
+        '<img src="https://goo.gl/e25sJG" alt="Porcelain Factory of Vista Alegre" height="150" width="150">' +
+        '<p>A Universidade de Brasília é uma universidade pública federal brasileira, com sede em Brasília, no Distrito Federal. A instituição possui 4 campi, sendo estes em Brasília, Planaltina, Gama, Ceilândia e Paranoá.</p>' +
+        '<div class="iw-subTitle">Contacts</div>' +
+        '<p>UnB - Brasília, DF, 70910-900'+
+        '<br>Phone.  (61) 3107-3300</p>'+
+        '</div>' +
+        '<div class="iw-bottom-gradient"></div>' +
+        '</div>'+
+        '</div>',2],
+        ['<div style="color: black;">'+'<p>Essa é a Saude.com</p>'+'</div>',1],
+        ['<div style="color: black;">'+'<p>Esse é o Ondas Burger</p>'+'</div>',1],
+        ['<div style="color: black;">'+'<p>Essa é o Açaí Casca</p>'+'</div>',1],
+        ['<div style="color: black;">'+'<p>Essa é o Essence Lounge Bar</p>'+'</div>',0]
+    ];
+
+
+
+
+
+
+
+
+
+
+
+
+
