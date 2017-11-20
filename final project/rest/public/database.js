4f102d1bbcc32b78b64e245a922b7d2f1092ec99
VerasUnB <thiago.wortnnes@gmail.com>
2017-11-20 22:14:18 +0000

    Adding new places

--- final project/rest/public/database.js
+++ final project/rest/public/database.js
@@ -12,6 +12,15 @@
         ['Ondas Burger',-15.661427,-47.8371979, 1],
         ['Açaí Casca',-15.756465,-47.891857, 1],
         ['Essence Lounge Bar',-15.814684,-47.889614, 0],
+        ['Gajah',-15.777048, -47.877806,0],
+        ['Delta',-15.770032, -47.891842,0],
+        ['Mangai',-15.817780, -47.836961,1],
+        ['Agrabah Hookah',-15.765046, -47.891338,0],
+        ['IESB',-15.834959, -47.912980,2],
+        ['IFB',-15.753933, -47.878799,2],
+        ['Bar Pôr do Sol',-15.760035, -47.879704,0],
+        ['Xique Xique',-15.764993, -47.890988,1]
+
     ];
 
     let informations = [
@@ -32,9 +41,17 @@
         ['<div style="color: black;">'+'<p>Essa é a Saude.com</p>'+'</div>',1],
         ['<div style="color: black;">'+'<p>Esse é o Ondas Burger</p>'+'</div>',1],
         ['<div style="color: black;">'+'<p>Essa é o Açaí Casca</p>'+'</div>',1],
-        ['<div style="color: black;">'+'<p>Essa é o Essence Lounge Bar</p>'+'</div>',0]
-    ];
+        ['<div style="color: black;">'+'<p>Essa é o Essence Lounge Bar</p>'+'</div>',0],
+        ['<div style="color: black;">'+'<p>Essa é o Gajah</p>'+'</div>',0],
+        ['<div style="color: black;">'+'<p>Essa é o Delta</p>'+'</div>',0],
+        ['<div style="color: black;">'+'<p>Essa é o Mangai</p>'+'</div>',1],
+        ['<div style="color: black;">'+'<p>Essa é o Agrabah Hookah</p>'+'</div>',0],
+        ['<div style="color: black;">'+'<p>Essa é o IESB</p>'+'</div>',2],
+        ['<div style="color: black;">'+'<p>Essa é o IFB</p>'+'</div>',2],
+        ['<div style="color: black;">'+'<p>Essa é o Bar Pôr do Sol</p>'+'</div>',0],
+        ['<div style="color: black;">'+'<p>Essa é o Xique Xique</p>'+'</div>',1]
 
+    ];
 
 
 
